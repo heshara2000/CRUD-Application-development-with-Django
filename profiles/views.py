7ebb0d3958e7076796e467303cd654533fa149f0
heshara2000 <hesharadananjanee@gmail.com>
2025-08-27 19:04:40 +0530

    add messages

--- profiles/views.py
+++ profiles/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Profile
 from .forms import ProfileForm
+from django.contrib import messages
+
 
 
 class ProfileListView(ListView):
@@ -20,6 +22,10 @@ class ProfileCreateView(CreateView):
     template_name = 'profiles/profile_form.html'
     success_url = reverse_lazy('profiles:profile-list')
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Profile created successfully.')
+        return super().form_valid(form)
+
 
 class ProfileUpdateView(UpdateView):
     model = Profile
@@ -27,8 +33,16 @@ class ProfileUpdateView(UpdateView):
     template_name = 'profiles/profile_form.html'
     success_url = reverse_lazy('profiles:profile-list')
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Profile updated successfully.')
+        return super().form_valid(form)
+
 
 class ProfileDeleteView(DeleteView):
     model = Profile
     template_name = 'profiles/profile_confirm_delete.html'
     success_url = reverse_lazy('profiles:profile-list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Profile deleted successfully.')
+        return super().delete(request, *args, **kwargs)
